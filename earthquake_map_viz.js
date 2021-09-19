b36f005fcc5f582684f7749ae77ae213de9cb3e9
tlim0018 <tlim0018@student.monash.edu>
2021-09-19 17:14:39 +0800

    correct typo

--- earthquake_map_viz.js
+++ earthquake_map_viz.js
@@ -1,4 +1,4 @@
 var vg_2 = "earthquake_magnitudes_map.vg.json";
-vegaEmbed("#symbolmap", vg_2).then(function(result) {
+vegaEmbed("#symbol_map", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
